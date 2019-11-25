80f8c9e4b7bd255e8c2f3b3c577a83a60b1dd1dd
Manuel Sousa <mlrcbsousa@gmail.com>
2019-11-25 11:01:52 +0100

    Add a jsonb and hstore column

---
+++ db/migrate/20191125095634_add_settings_to_posts.rb
@@ -0,0 +1,9 @@
+class AddSettingsToPosts < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :settings, :jsonb, null: false, default: {}
+    add_index  :users, :settings, using: :gin
+
+    add_column :users, :conditions, :hstore
+    add_index  :users, :conditions, using: :gin
+  end
+end
